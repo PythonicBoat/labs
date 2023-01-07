f9697b83dfe1e864e21e0976c3acb793f1bfd7d4
Yashvardhan Singh <68675629+PythonicBoat@users.noreply.github.com>
2023-01-07 15:20:06 +0530

    Create q1.c

---
+++ lab18/q1.c
@@ -0,0 +1,3 @@
+/*
+WAP to read roll number, name, gender and marks data of n number of students  from user and store them in a file. If the file previously exists, add the information of n students
+*/
